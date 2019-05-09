97067666158927e11c1f3cab4a94d9233b11806a
Aleksandar Trninkov <atrninkov1@studentmail.nait.ca>
2019-05-09 12:34:45 -0600

    added basic addition and subtraction functionality

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,14 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
   displayValue = "";
-  
+  firstValue = 0;
+  clearDisplay = false;
+  firstValueInputed = false;
   constructor() { }
 
   ngOnInit() {
   }
 
   public PressNum(number) {
+    if (this.clearDisplay) {
+      this.displayValue = "";
+      this.clearDisplay = false;
+    }
     this.displayValue+=number;
   }
 
+  public Clear(){
+    this.displayValue="";
+  }
+
+  public Operation(operation){
+    switch (operation) {
+      case "+":
+        if (this.displayValue == "") {
+          this.displayValue="0";
+        }
+        this.firstValue+= +this.displayValue;
+        this.displayValue=this.firstValue.toString();
+        this.clearDisplay = true;
+        break;
+      case "-":
+          if (this.displayValue == "") {
+            this.displayValue="0";
+          }
+          if (!this.firstValueInputed) {
+            this.firstValue = +this.displayValue;
+            this.clearDisplay = true;
+          }
+          else{
+            this.firstValue-= +this.displayValue;
+            this.displayValue=this.firstValue.toString();
+            this.clearDisplay = true;
+          }
+          break;
+      
+      case "X":
+          if (this.displayValue == "") {
+            this.displayValue="0";
+          }
+          this.firstValue*= +this.displayValue;
+          this.displayValue=this.firstValue.toString();
+          this.clearDisplay = true;
+          break;
+      
+      case "÷":
+          if (this.displayValue == "") {
+            this.displayValue="0";
+          }
+          this.firstValue/= +this.displayValue;
+          this.displayValue=this.firstValue.toString();
+          this.clearDisplay = true;
+          break;
+      default:
+        break;
+    }
+    this.firstValueInputed = true;
+  }
 }
