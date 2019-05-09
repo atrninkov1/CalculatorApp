a2cc87519b4fc3ac464b3d7e529d38ba655af882
Aleksandar Trninkov <atrninkov1@studentmail.nait.ca>
2019-05-09 12:07:02 -0600

    Made basic Calculator application

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import {RouterModule, Routes} from '@angular/router'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    CalculatorComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:"about", component:AboutComponent},
+      {path:"calculator", component:CalculatorComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
