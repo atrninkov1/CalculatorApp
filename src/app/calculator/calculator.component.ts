be42cd3f57f90e8d9e458e31398c2118b0da8af7
Aleksandar Trninkov <atrninkov1@studentmail.nait.ca>
2019-05-18 14:07:06 -0600

    made backspace function and added keyboard events

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,16 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, HostListener, Directive, Input} from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent implements OnInit {
+  @HostBinding('tabindex') tabIndex = 1;
+  @HostListener('keyup', ['$event'])
+  onKeyUp(kbdEvent: any): void {
+    if (+kbdEvent.key) {
+      this.PressNum(kbdEvent.key)      
+    }
+    switch (kbdEvent.key) {
+      case "Enter":
+          this.Operation("");        
+      break;
+      case "+":
+          this.Operation("+");        
+      break;
+      case "-":
+          this.Operation("-");        
+      break;
+      case "/":
+          this.Operation("÷");        
+      break;
+      case "*":
+          this.Operation("X");        
+      break;
+      case "%":
+          this.Operation("%");        
+      break;
+      case "c":
+          this.Clear();        
+      break;
+      case "Backspace":
+          this.ComplexOperation("◄");        
+      break;
+      case "Backspace":
+          this.ComplexOperation("◄");        
+      break;
+      case "0":
+          this.PressNum(0);        
+      break;
+      case ".":
+          this.PressNum(".");        
+      break;
+      case "s":
+        this.ComplexOperation("√");
+        break;
+      case "S":
+        this.ComplexOperation("x²");
+        break;
+      case "C":
+        this.ComplexOperation("x³");
+      break;      
+      case "i":
+        this.ComplexOperation("1/X");
+      break;
+      default:
+        break;
+    }
+  }
+ 
+
+
   displayValue = "";
   firstValue = 0.0;
   clearDisplay = false;
   firstValueInputed = false;
   lastOperation = "";
+  previousDisplay = "";
+  historicDisplay = "";
   constructor() { }
 
   ngOnInit() {
@@ -30,6 +91,7 @@ export class CalculatorComponent implements OnInit {
     this.clearDisplay = false;
     this.firstValueInputed = false;
     this.lastOperation = "";
+    this.previousDisplay = "";
   }
 
   public ClearEntry(){
@@ -43,8 +105,6 @@ export class CalculatorComponent implements OnInit {
           this.displayValue="0";
         }
         this.firstValue+= +this.displayValue;
-        this.displayValue=this.firstValue.toString();
-        this.clearDisplay = true;
         break;
       case "-":
           if (this.displayValue == "") {
@@ -56,8 +116,6 @@ export class CalculatorComponent implements OnInit {
           }
           else{
             this.firstValue-= +this.displayValue;
-            this.displayValue=this.firstValue.toString();
-            this.clearDisplay = true;
           }
           break;
       
@@ -71,8 +129,6 @@ export class CalculatorComponent implements OnInit {
         }
         else{
           this.firstValue*= +this.displayValue;
-          this.displayValue=this.firstValue.toString();
-          this.clearDisplay = true;
         }
       break;
       
@@ -86,8 +142,6 @@ export class CalculatorComponent implements OnInit {
         }
         else{
           this.firstValue/= +this.displayValue;
-          this.displayValue=this.firstValue.toString();
-          this.clearDisplay = true;
         }
       break;
 
@@ -101,8 +155,6 @@ export class CalculatorComponent implements OnInit {
         }
         else{
           this.firstValue%= +this.displayValue;
-          this.displayValue=this.firstValue.toString();
-          this.clearDisplay = true;
         }
       break;
       default:      
@@ -112,6 +164,14 @@ export class CalculatorComponent implements OnInit {
     }
     this.firstValueInputed = true;
     this.lastOperation = operation;
+    var previous = this.displayValue + this.lastOperation;
+    this.previousDisplay += previous;
+    if (operation == "") {      
+      this.historicDisplay += (this.previousDisplay + "\n");
+      this.previousDisplay = "";
+    }
+    this.displayValue=this.firstValue.toString();
+    this.clearDisplay = true;
   }
 
   public ComplexOperation(operation){
@@ -121,10 +181,46 @@ export class CalculatorComponent implements OnInit {
           this.displayValue="0";
         }
         this.displayValue = Math.sqrt(+this.displayValue).toString();
+        break;
+      case "x²":
+        if (this.displayValue == "") {
+          this.displayValue="0";
+        }
+        this.displayValue = (+this.displayValue * +this.displayValue).toString();
       break;
+      case "x³":
+        if (this.displayValue == "") {
+          this.displayValue="0";
+        }
+        this.displayValue = (+this.displayValue * +this.displayValue * +this.displayValue).toString();
+      break;
+      
+      case "1/X":
+        if (this.displayValue == "") {
+          this.displayValue="0";
+        }
+        this.displayValue = (1 / +this.displayValue).toString();
+      break;
+      
+      case "±":
+        if (this.displayValue == "") {
+          this.displayValue="0";
+        }
+        this.displayValue = (+this.displayValue * -1).toString();
+      break;
+      case "◄":
+        this.displayValue = this.displayValue.substring(0, this.displayValue.length-1)
+      break;
+      
     
       default:
         break;
+    }
+  }
+
+  onKeydown(event) {
+    if (event.key === "Enter") {
+      console.log(event);
     }
   }
 }
